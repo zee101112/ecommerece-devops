8dd139abccd39a4b95c15ab0cd8ba825856ff78d
Zahid Zeeshan <zzeeshan0503@gmail.com>
2026-01-21 01:25:04 +0000

    Fix static paths and settings

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -79,6 +79,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+}
 
 # Media files
 MEDIA_URL = '/media/'
