287b974cb5e1476ce7d28b87a1d5b19eaa95f30c
Zahid Zeeshan <zzeeshan0503@gmail.com>
2026-01-21 02:53:54 +0000

    Use PostgreSQL via DATABASE_URL

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -61,12 +62,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(default=f"sqlite:///{BASE_DIR/'db.sqlite3'}", conn_max_age=600)
 }
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
