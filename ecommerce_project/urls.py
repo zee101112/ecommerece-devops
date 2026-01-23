5f82d9fbde2fd0b1e94f39cf748ce975e1612cbb
Zahid Zeeshan <zzeeshan0503@gmail.com>
2026-01-23 16:37:01 +0000

    Serve static and media when DEBUG is false

--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', include('shop.urls')),
 ]
 
-# Serve media files in all environments (local + Render)
+# Serve media and static files (works even when DEBUG=False)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
